70dcba0c31b8ead7df8349c14cb4dd4254b80dd9
sunan-rabbi <sunanrabbi1918@outlook.com>
2024-10-29 15:50:38 +0600

    show post

--- src/app/(withCommonLayout)/blogs/component/BlogCard.tsx
+++ src/app/(withCommonLayout)/blogs/component/BlogCard.tsx
@@ -1,14 +1,16 @@
+import dayjs from 'dayjs';
 import React from 'react';
 
-const BlogCard = () => {
+const BlogCard = ({ post }: { post: any }) => {
+    const formattedDate = dayjs(post.createdAt).format('YYYY-MM-DD')
     return (
         <div className='border w-96 m-4 p-4 rounded-md border-black-400'>
-            <h3 className='text-lg font-bold'>This is post Title</h3>
+            <h3 className='text-lg font-bold'>{post.title}</h3>
             <div className='text-state-500 text-sm text-gray-700 flex justify-between py-2'>
-                <p>Author Name</p>
-                <p>Date: </p>
+                <p>Author Name: {post.author.name}</p>
+                <p>Date: {formattedDate}</p>
             </div>
-            <p className='text-sm'>This is Content</p>
+            <p className='text-sm'>{post.content}</p>
         </div>
     );
 };
--- src/app/(withCommonLayout)/blogs/page.tsx
+++ src/app/(withCommonLayout)/blogs/page.tsx
@@ -5,15 +5,16 @@ import { gql, useQuery } from '@apollo/client';
 
 const GET_POSTS = gql`
     query allPost{
-      posts {
-        id
-        title
-        content
-        author {
-          name
-          email
+        posts {
+            id
+            title
+            content
+            createdAt
+            author {
+              name
+              email
+            }
         }
-      }
     }
 `
 
@@ -22,12 +23,18 @@ const Posts = () => {
     if (loading) {
         return <p>Loading...</p>;
     }
-    console.log(data);
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
     return (
         <div>
             <h1 className='text-center font-bold text-5xl my-4 pb-4'>Posts</h1>
             <hr />
-            <BlogCard />
+            <div className='flex flex-wrap justify-center items-center'>
+                {
+                    data.posts.map((post: any) => <BlogCard key={post.id} post={post} />)
+                }
+            </div>
         </div>
     );
 };
